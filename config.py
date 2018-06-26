e0ef6aaa8dc3c214f8b3691470a8d13b958df4f5
gregoryReneCorail <40594318+gregoryReneCorail@users.noreply.github.com>
2018-06-26 11:56:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Configuration(object):
     HOST = '140.86.15.104'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://' \
-            + os.getenv('MYSQLCS_USER_NAME','root') + ':' \
-            + os.getenv('MYSQLCS_USER_PASSWORD','password') + '@' \
+            + os.getenv('MYSQLCS_USER_NAME','captain') + ':' \
+            + os.getenv('MYSQLCS_USER_PASSWORD','welcome1') + '@' \
             + os.getenv('MYSQLCS_CONNECT_STRING', \
-            '127.0.0.1:3306/sample_db')
+            '127.0.0.1:3306/deathstar')
