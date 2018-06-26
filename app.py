a0602450550bbcb89bc48042057ef83b2ebf71d2
gregoryReneCorail <40594318+gregoryReneCorail@users.noreply.github.com>
2018-06-26 12:10:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://140.86.15.104:3000/fighters/45/9/yellow/gregoryReneCorail").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/320/650/yellow/gregoryReneCorail").text)
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
